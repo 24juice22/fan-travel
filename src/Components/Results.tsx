a32e6b4fdd97bf9a9fc2ab6c835ee297ef501208
24juice22 <mike.james.nunn@gmail.com>
2022-11-13 09:23:49 -0500

    Create Results Component
    Create a component to Display a list of events based on the keyword entered

---
+++ src/Components/Results.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import { IResults } from "../interfaces"
+import Card from "./Card"
+
+interface ResultsProps {
+    results: IResults[];
+    keyword: string;
+}
+
+const Results: FC<ResultsProps> = ({ results, keyword }) => { 
+  console.log(results);
+  return (
+    <div className="results">
+      <h2 className="results-title">{keyword}</h2>
+      {results.map((item: IResults) => <Card results={item}/>)}
+    </div>
+  )
+}
+
+export default Results
