3340920f0f652bff2bc027489f39fdd5b02b2158
24juice22 <mike.james.nunn@gmail.com>
2022-11-27 21:33:25 -0500

    Update Results Title
    Add verbiage to results title and adjust styling

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -11,11 +11,13 @@ const Results: FC<ResultsProps> = ({ results, keyword }) => {
   console.log(results);
   return (
     <div className="results">
-      <h2 className="results-title">{keyword}</h2>
-      
-        <div className="row">
-          {results.map((item: IResults) => <Card results={item} keyword={keyword}/>)}
-        </div>
+      <h2 className="results-title">
+        Upcoming games involving 
+        <span> {keyword.toUpperCase()}</span>
+      </h2>
+      <div className="row">
+        {results.map((item: IResults) => <Card results={item} keyword={keyword}/>)}
+      </div>
 
     </div>
   )
